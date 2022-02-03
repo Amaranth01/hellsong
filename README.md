ccc995f4314c6c6248870791f1f9abba33c6ddbc
Amaranth01 <vanessa.nectoux@gmail.com>
2022-02-03 16:17:42 +0100

    front first part

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hellsong
+
+D'abord le header avec le menu 3 trucs : accueil / Nos groupes / Contact
---
+++ partials/headers.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hellsong</title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+
+<header>
+    <img src="/assets/img/logo_site500x250.png" alt="Logo du site Hellsong" id="logo">
+    <h1 class="title">Hellsong.fr
+        <br>
+        Un son d'enfer !</h1>
+</header>
+
+<div id="content-menu">
+    <nav>
+        <ul>
+            <li><a href="/pages/home.php">Accueil</a></li>
+            <li class="scrolling"><a href="/pages/ourGroups.php">Nos groupes</a>
+                <ul class="little">
+                    <li><a href="/pages/allGroups/bad-taste.php">Bad Taste</a></li>
+                    <li><a href="/pages/allGroups/bad-taste.php">Boars</a></li>
+                    <li><a href="/pages/allGroups/dagara.php">DAGARA</a></li>
+                    <li><a href="/pages/allGroups/smashItCombo.php">Smash It Combo</a></li>
+                    <li><a href="/pages/allGroups/sangdragon.php">Sangdragon</a></li>
+                </ul>
+            </li>
+            <li class="scrolling"><a href="/pages/contact.php">Contact</a>
+                <ul class="little">
+                    <li><a href="/pages/contact.php">Formulaire de contact</a></li>
+<!--                    à voir ce qu'il veut exactement-->
+                    <li><a href="/pages/social.php">Réseaux sociaux</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+</div>
